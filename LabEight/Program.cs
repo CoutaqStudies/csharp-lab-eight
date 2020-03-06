a38c3e0eee66cc3993aa475478437082926372e2
coutaq <coutaq@gmail.com>
2020-03-06 15:45:21 +0300

    new class

--- LabEight/Program.cs
+++ LabEight/Program.cs
@@ -13,28 +13,24 @@ namespace LabEight
             String data = System.IO.File.ReadAllText(OsHelper.CompatiblePath(filepath, true));
             List<ListItem> strings = new List<ListItem>();
             int index = 0;
+
             foreach (String s in data.Split('\n'))
             {
                 strings.Add(new ListItem(index, s));
                 index++;
             }
-            
+
             Console.Write("Search: ");
             String search = Console.ReadLine();
-            List<ListItem> results = new List<ListItem>();
+            
             Stopwatch stpw = new Stopwatch();
+            TimeSpan ts;
             stpw.Start();
-            results = strings.FindAll(s => s.value.Equals(search));
+            
             stpw.Stop();
-            if (results.Count == 0)
-                Console.WriteLine("Not Found.");
-            List<int> indexes = new List<int>();
-            foreach (ListItem li in results)
-            {
-                indexes.Add(li.index);
-                Console.Write(li.index);
-            }
-            Console.WriteLine("Elapsed time: "+stpw.ElapsedMilliseconds);
+            ts = stpw.Elapsed;
+            
+            Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
         }
     }
 }
---
+++ LabEight/Search.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace LabEight
+{
+    public class Search
+    {
+        public int[] LinearSearch(String searchFor, List<ListItem> list)
+        {
+
+            List<ListItem> results = new List<ListItem>();
+            results = list.FindAll(s => s.value.Equals(searchFor));
+
+            if (results.Count == 0)
+                throw new Exception();
+
+            List<int> indexes = new List<int>();
+
+            foreach (ListItem li in results)
+                indexes.Add(li.index);
+
+            return indexes.ToArray();
+        }
+    }
+}
