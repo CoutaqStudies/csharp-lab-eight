f8a9536ee5410db8bf30341ea7a9eaaa45755830
coutaq <coutaq@gmail.com>
2020-03-06 15:53:26 +0300

    linear search done yay now the hard part..

--- LabEight/Program.cs
+++ LabEight/Program.cs
@@ -12,7 +12,7 @@ namespace LabEight
             String filepath = "sorted.dat";
             String data = System.IO.File.ReadAllText(OsHelper.CompatiblePath(filepath, true));
             List<ListItem> strings = new List<ListItem>();
-            int index = 0;
+            int index = 1;
 
             foreach (String s in data.Split('\n'))
             {
@@ -20,16 +20,24 @@ namespace LabEight
                 index++;
             }
 
-            Console.Write("Search: ");
+            Console.Write("Linear search: ");
             String search = Console.ReadLine();
-            
+
+            int[] indexes;
             Stopwatch stpw = new Stopwatch();
             TimeSpan ts;
+
             stpw.Start();
-            
+                indexes = Search.LinearSearch(search, strings);
             stpw.Stop();
             ts = stpw.Elapsed;
-            
+            if (indexes.Length != 0)
+                foreach (int i in indexes)
+                {
+                    Console.WriteLine(i);
+                }
+            else
+                Console.WriteLine("Not found.");
             Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
         }
     }
--- LabEight/Search.cs
+++ LabEight/Search.cs
@@ -6,15 +6,11 @@ namespace LabEight
 {
     public class Search
     {
-        public int[] LinearSearch(String searchFor, List<ListItem> list)
+        public static int[] LinearSearch(String searchFor, List<ListItem> list)
         {
 
             List<ListItem> results = new List<ListItem>();
             results = list.FindAll(s => s.value.Equals(searchFor));
-
-            if (results.Count == 0)
-                throw new Exception();
-
             List<int> indexes = new List<int>();
 
             foreach (ListItem li in results)
