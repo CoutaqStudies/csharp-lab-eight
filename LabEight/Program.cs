a0858f84b2a5c6a4f903208bd8ba95bf6f7956c3
Michael Melikov <coutaq@gmail.com>
2020-04-06 18:09:01 +0300

    tried to do something out of boredom.. should've played witcher instead

--- LabEight/Program.cs
+++ LabEight/Program.cs
@@ -11,33 +11,59 @@ namespace LabEight
         {
             String filepath = "sorted.dat";
             String data = System.IO.File.ReadAllText(OsHelper.CompatiblePath(filepath, true));
-            List<ListItem> strings = new List<ListItem>();
-            int index = 1;
+            List<String> strings = new List<String>();
+            int index = 0;
 
             foreach (String s in data.Split('\n'))
             {
-                strings.Add(new ListItem(index, s));
+                strings.Add(s);
+            }
+
+            int[] nums =new int[strings.Count];
+
+            foreach (String s in strings)
+            {
+                nums[index] = int.Parse(s);
                 index++;
             }
 
             Console.Write("Linear search: ");
-            String search = Console.ReadLine();
+            int searchFor = int.Parse(Console.ReadLine());
 
             int[] indexes;
             Stopwatch stpw = new Stopwatch();
             TimeSpan ts;
 
             stpw.Start();
-                indexes = Search.LinearSearch(search, strings);
+            int found = +Search.LinearSearch(nums, searchFor);
+            if(found!=-1) 
+                Console.WriteLine("array["+found+"] = "+searchFor);
+            else
+                Console.WriteLine("Not found!");
             stpw.Stop();
             ts = stpw.Elapsed;
-            if (indexes.Length != 0)
-                foreach (int i in indexes)
-                {
-                    Console.WriteLine(i);
-                }
+            Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
+
+            Console.WriteLine("Binary:");
+            stpw.Reset();
+            found = +Search.BinarySearch(nums, searchFor);
+            if (found != -1)
+                Console.WriteLine("array[" + found + "] = " + searchFor);
             else
-                Console.WriteLine("Not found.");
+                Console.WriteLine("Not found!");
+            stpw.Stop();
+            ts = stpw.Elapsed;
+            Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
+
+            Console.WriteLine("Interpolation:");
+            stpw.Reset();
+            found = +Search.InterpolationSearch(nums, searchFor);
+            if (found != -1)
+                Console.WriteLine("array[" + found + "] = " + searchFor);
+            else
+                Console.WriteLine("Not found!");
+            stpw.Stop();
+            ts = stpw.Elapsed;
             Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
         }
     }
--- LabEight/Search.cs
+++ LabEight/Search.cs
@@ -6,17 +6,50 @@ namespace LabEight
 {
     public class Search
     {
-        public static int[] LinearSearch(String searchFor, List<ListItem> list)
+        public static int LinearSearch(int[] array, int searchFor)
         {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == searchFor)
+                    return i;
+                return -1;
+        }
+        public static int BinarySearch(int[] array, int searchFor)
+        {
+            if (searchFor > array[array.Length-1])
+                return -1;
+            int l = 0, r = array.Length-1;
+            do
+            {
+                int m = (l + r) / 2;
+                if (array[m] < searchFor)
+                    l = m + 1;
+                else if(array[m]>searchFor)
+                    r = m - 1;
+                else
+                    return m;
+            } while (l <= r);
+            return -1;
+        }
+        public static int InterpolationSearch(int[] array, int searchFor)
+        {
+            int l = 0, r = array.Length - 1;
 
-            List<ListItem> results = new List<ListItem>();
-            results = list.FindAll(s => s.value.Equals(searchFor));
-            List<int> indexes = new List<int>();
+            if (array[l] == searchFor)
+                return l;
+            if (array[r] == searchFor)
+                return r;
 
-            foreach (ListItem li in results)
-                indexes.Add(li.index);
+            while (r >= l)
+            {
+                int mid = l + ((searchFor - array[l]) * (r - l)) / (array[r] - array[l]);
+                if (array[mid] > searchFor)
+                    r = mid - 1;
+                else if (array[mid] < searchFor)
+                    l = mid + 1;
+                else return mid;
+            }
 
-            return indexes.ToArray();
+            return -1;
         }
     }
 }
