91df845fcb4c21f8628e1ace767d3c52901c9fe2
Michael Melikov <coutaq@gmail.com>
2020-04-30 12:54:22 +0300

    implemented prefix calculator

--- LabEight/Program.cs
+++ LabEight/Program.cs
@@ -9,64 +9,77 @@ namespace LabEight
     {
         static void Main(string[] args)
         {
+            string text = "ABABDABACDABABCABAB";
+            string pattern = "ABABCABAB";
+            Console.WriteLine(SubstringSearch.KMPSearch(text, pattern).Length);
+            foreach (var VARIABLE in SubstringSearch.KMPSearch(text, pattern))
+            {
+                Console.Write(VARIABLE+" ");
+            }
+            /*int[] table = SubstringSearch.KMP_Table(test);
+            foreach (var VARIABLE in table)
+            {
+                Console.WriteLine(VARIABLE);
+            }*/
+
             //search for 555 and 695803 
-            String filepath = "sorted.dat";
-            String data = System.IO.File.ReadAllText(OsHelper.CompatiblePath(filepath, true));
-            List<String> strings = new List<String>();
-            int index = 0;
-
-            foreach (String s in data.Split('\n'))
-            {
-                strings.Add(s);
-            }
-
-            int[] nums =new int[strings.Count];
-
-            foreach (String s in strings)
-            {
-                nums[index] = int.Parse(s);
-                index++;
-            }
-
-            Console.Write("Linear search: ");
-            int searchFor = int.Parse(Console.ReadLine());
-
-
-            int[] indexes;
-            Stopwatch stpw = new Stopwatch();
-            TimeSpan ts;
-
-            stpw.Start();
-            int found = +Search.LinearSearch(nums, searchFor);
-            if(found!=-1) 
-                Console.WriteLine("array["+found+"] = "+searchFor);
-            else
-                Console.WriteLine("Not found!");
-            stpw.Stop();
-            ts = stpw.Elapsed;
-            Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
-
-            Console.WriteLine("Binary:");
-            stpw.Reset();
-            found = +Search.BinarySearch(nums, searchFor);
-            if (found != -1)
-                Console.WriteLine("array[" + found + "] = " + searchFor);
-            else
-                Console.WriteLine("Not found!");
-            stpw.Stop();
-            ts = stpw.Elapsed;
-            Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
-
-            Console.WriteLine("Interpolation:");
-            stpw.Reset();
-            found = +Search.InterpolationSearch(nums, searchFor);
-            if (found != -1)
-                Console.WriteLine("array[" + found + "] = " + searchFor);
-            else
-                Console.WriteLine("Not found!");
-            stpw.Stop();
-            ts = stpw.Elapsed;
-            Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
+            /* String filepath = "sorted.dat";
+             String data = System.IO.File.ReadAllText(OsHelper.CompatiblePath(filepath, true));
+             List<String> strings = new List<String>();
+             int index = 0;
+ 
+             foreach (String s in data.Split('\n'))
+             {
+                 strings.Add(s);
+             }
+ 
+             int[] nums =new int[strings.Count];
+ 
+             foreach (String s in strings)
+             {
+                 nums[index] = int.Parse(s);
+                 index++;
+             }
+ 
+             Console.Write("Linear search: ");
+             int searchFor = int.Parse(Console.ReadLine());
+ 
+ 
+             int[] indexes;
+             Stopwatch stpw = new Stopwatch();
+             TimeSpan ts;
+ 
+             stpw.Start();
+             int found = +Search.LinearSearch(nums, searchFor);
+             if(found!=-1) 
+                 Console.WriteLine("array["+found+"] = "+searchFor);
+             else
+                 Console.WriteLine("Not found!");
+             stpw.Stop();
+             ts = stpw.Elapsed;
+             Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
+ 
+             Console.WriteLine("Binary:");
+             stpw.Reset();
+             found = +Search.BinarySearch(nums, searchFor);
+             if (found != -1)
+                 Console.WriteLine("array[" + found + "] = " + searchFor);
+             else
+                 Console.WriteLine("Not found!");
+             stpw.Stop();
+             ts = stpw.Elapsed;
+             Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);
+ 
+             Console.WriteLine("Interpolation:");
+             stpw.Reset();
+             found = +Search.InterpolationSearch(nums, searchFor);
+             if (found != -1)
+                 Console.WriteLine("array[" + found + "] = " + searchFor);
+             else
+                 Console.WriteLine("Not found!");
+             stpw.Stop();
+             ts = stpw.Elapsed;
+             Console.WriteLine("Time: " + ts.Seconds + ":" + ts.Milliseconds);*/
         }
     }
 }
---
+++ LabEight/SubstringSearch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabEight
+{
+    public class SubstringSearch
+    {
+        public static int[] KMPSearch(String text, String pattern)
+        {
+            List<int> matches = new List<int>();
+            int[] prefix = LongestPrefix(pattern);
+            int i = 0, j = 0;
+            while (i < pattern.Length)
+            {
+                if (pattern[j] == text[i])
+                {
+                    j++;
+                    i++;
+                }
+                if (j == pattern.Length)
+                {
+                    Console.WriteLine("test");
+                    matches.Add(i-j);
+                    j = prefix[j - 1];
+                }
+                else if (i < pattern.Length && pattern[j] != text[i])
+                {
+                    if (j > 0)
+                        j = prefix[j - 1];
+                    else
+                        i++;
+                }
+            }
+            return matches.ToArray();
+        }
+        public static int[] LongestPrefix(string pattern)
+        {
+            int[] prefix = new int[pattern.Length];
+            int length = 0;
+            for (int i = 1; i < pattern.Length; i++)
+            {
+                if (pattern[i] == pattern[length])
+                {
+                    length++;
+                    prefix[i] = length;
+                }
+                else
+                {
+                    if (length>0)
+                    {
+                        length = prefix[length - 1];
+                        i--;
+                    }
+                    else
+                    {
+                        prefix[i] = length;
+                    }
+                }
+            }
+            return prefix;
+        }
+    }
+}
